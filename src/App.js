de3f9c0675140fd4f6b668444c36700c6ced1721
Al3ncar <alencar29htcss@gmail.com>
2023-05-15 12:39:43 -0300

    Count upgrade

--- src/App.js
+++ src/App.js
@@ -1,33 +1,62 @@
-import React, {Component} from 'react'
-import './App.css'
+// import React, {Component} from 'react'
+// import './App.css'
 
-export default class App extends Component{
-  state = {
-    number:0,
+// export default class App extends Component{
+//   state = {
+//     number:0,
+//   }
+//   soma = () => {
+//     this.setState({
+//       number: this.state.number === 10 ? this.state.number=10: this.state.number+1,
+//     })
+//   }
+//   adiçao = () => {
+//     this.setState({
+//       number: this.state.number === 0 ? this.state.number=0: this.state.number-1
+//     })
+//   }
+//   render(){
+//     return(
+//       <div className='calc'>
+//         <div className='calc-container'>
+//           <div className='result'>
+//             <h1>{this.state.number}</h1>
+//           </div>
+//           <div className='options'>
+//             <button onClick={this.soma}>+1</button>
+//             <button onClick={this.adiçao}>-1</button>
+//           </div>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+
+import React, {Component} from "react"
+
+export default class App extends Component {
+  state = { number: 0}
+
+  Add = () => {
+    this.setState({ number: this.state.number + 1 })
   }
-  soma = () => {
-    this.setState({
-      number: this.state.number === 10 ? this.state.number=10: this.state.number+1,
-    })
-  }
-  adiçao = () => {
-    this.setState({
-      number: this.state.number === 0 ? this.state.number=0: this.state.number-1
-    })
-  }
+
+
   render(){
     return(
-      <div className='calc'>
-        <div className='calc-container'>
-          <div className='result'>
-            <h1>{this.state.number}</h1>
+      <section>
+        <div>
+          <div>
+            <p>{this.state.number}</p>
           </div>
-          <div className='options'>
-            <button onClick={this.soma}>+1</button>
-            <button onClick={this.adiçao}>-1</button>
+          <div>
+            <button onClick={this.Add}>Somar</button>
+            <button onClick={this.decrease}>Diminuir</button>
+            <button>x2</button>
           </div>
         </div>
-      </div>
+      </section>
     )
   }
 }
